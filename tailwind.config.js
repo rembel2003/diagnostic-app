2817b8c5568d5c163016b813bd810b5000bfeab9
rembel2003 <yura.kkrut.2003@gmail.com>
2023-12-17 20:40:58 +0200

    first page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-red': '#E95258',
+        'regal-yellow': '#F2C94C',
+        'instagram': '#C33A7C',
+        'telefon': '#32495E',
+        'messanger': '#4598FF',
+        'whatsapp': '#14AC51',
+        'backtotext':'#F9F9F9',
+        'backforquestion':'#49647D'
+      },
+    },
+  },
+  plugins: [],
+}
+
